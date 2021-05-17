54d2cf8f6d7a2edb7bd806adb6536f6a960f488e
Viswa Kumar S <visw2644@global.chainsys.com>
2021-05-17 16:53:27 +0530

    moved files

--- src/test/java/in/viswa/trading/validator/CurrencyConverterTest.java
+++ src/test/java/in/viswa/trading/validator/CurrencyConverterTest.java
@@ -8,12 +8,12 @@ public class CurrencyConverterTest {
 		double dollar=88;
 		String inr ="inr";
 		double actual=CurrencyConverter.converter(dollar, inr);
-		System.out.println( dollar+" is equal to " +actual+"Rs");
+		System.out.println( dollar+" is equal to " +actual+" Rs");
          
 		//test2
 				double dollar1=74;
 				String inr1 ="dollar";
 				double actual1=CurrencyConverter.converter(dollar1, inr1);
-				System.out.println( dollar1 +" is equal to "+actual1+"$");
+				System.out.println( dollar1 +" is equal to "+actual1+" $");
 	}
 }
